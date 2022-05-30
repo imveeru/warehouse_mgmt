20d4f36925d49e23cd61bd311bc103c85c526e5c
imvee <vind.veeru@gmail.com>
2022-05-30 23:53:44 +0530

    Added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './pages/Home/Home'
-// import Register from './pages/Register/Register'
-// import { AuthProvider } from "./components/Auth/AuthProvider";
-// import { Routes, Route,Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Home/>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </div>
   );
 }
 
