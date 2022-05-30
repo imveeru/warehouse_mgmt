4afb4ea5e33e4f2d636fcdc70cf1455f6814e0d9
imvee <vind.veeru@gmail.com>
2022-05-31 00:39:06 +0530

    Setup private context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Home from './pages/Home/Home'
 import Register from './pages/Register/Register'
+import { AuthProvider } from "./context/AuthContext"
+import PrivateRoute from './routes/PrivateRoute'
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <PrivateRoute path="/" element={<Home/>}/>
         <Route exact path="/register" element={<Register/>}/>
       </Routes>
     </div>
