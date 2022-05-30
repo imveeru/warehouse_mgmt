3eb36346370e7321403ad4af9a2e14fe76491c3b
imvee <vind.veeru@gmail.com>
2022-05-31 01:00:28 +0530

    Register component

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Route,history,Navigate  } from "react-router-dom";
+import Home from '../Home/Home'
+import { useAuth } from "../../context/AuthContext"
 
 function Register() {
+
+  const { signup } = useAuth()
+
+  const[error,setError]=useState("")
+  const[isLoading,setLoading]=useState(false)
+
+  const handleSubmit = async (e) => {
+    
+    e.preventDefault();    
+    const { email, password } = e.target.elements;
+
+    try {
+      setError("")
+      setLoading(true)
+      await signup(email.value, password.value)
+      return <Navigate to="/" replace={true}/>
+    } catch (error) {
+      setError(error);
+    }
+
+    setLoading(false)
+  };
+  
+
   return (
-    <div>Register</div>
+    <>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Email" />
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{error}</p>
+    </>
   )
 }
 
