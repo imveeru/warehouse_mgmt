d737978968795722ac58359ce4695f7a5394856f
imvee <vind.veeru@gmail.com>
2022-06-05 23:34:19 +0530

    Fogot password component

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useAuth } from "../../context/AuthContext"
 
 function ForgotPassword() {
+
+    const { resetPassword } = useAuth()
+    const [error, setError] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleSubmit=async (e) =>{
+
+        
+        e.preventDefault()
+
+        const { email } = e.target.elements
+
+        try {
+        setMessage("")
+        setError("")
+        await resetPassword(email.value)
+        setMessage("Check your inbox for further instructions")
+        } catch {
+        setError("Failed to reset password")
+        }
+
+    }
+
+
+
     return (
         <div>
             <p>Forgot Password</p>
-            <form>
-                <input type="email" placeholder="Enter your email address"></input>
+            <form onSubmit={handleSubmit}>
+                <input type="email" placeholder="Enter your email address" name="email"></input>
                 <button type="submit">Send mail</button>
             </form>
+            <p>{message}</p>
         </div>
     )
 }
