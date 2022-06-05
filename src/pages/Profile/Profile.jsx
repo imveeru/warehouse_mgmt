cb5355e82c67db4a5cbcd18aae2cb2058977c494
imvee <vind.veeru@gmail.com>
2022-06-05 20:49:06 +0530

    Profile component

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 function Profile({userID}) {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
   return (
     <div>
-        
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("name")} type="text" className="" placeholder="Enter your name" name="name"></input>
+            <input {...register("address")} type="text" className="" placeholder="Enter your address" name="address"></input>
+            <input {...register("contact")} type="text" className="" placeholder="Enter your mobile number" name="contact"></input>
+            <select {...register("type")} type="text" className=""  name="contact">
+                <option default disabled value="Choose your user type"></option>
+                <option value="Restaurant"></option>
+                <option value="Grocery Store"></option>
+            </select>
+            <input {...register("userSince")} type="text" className="" hidden value={new Date().getFullYear()}/>
+        </form>
+        <p>{}</p>
     </div>
   )
 }
