992c38823339090209673d3a8c9218ed2bb7aa49
imvee <vind.veeru@gmail.com>
2022-06-05 20:55:18 +0530

    Profile component

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,7 +5,10 @@ function Profile({userID}) {
 
     const { register, handleSubmit } = useForm();
     const[userData,setUserData]=useState()
-    const onSubmit = data => setUserData(data);
+    const onSubmit = (data) => {
+        console.log(data);
+        setUserData(data)
+    }
 
   return (
     <div>
@@ -13,12 +16,13 @@ function Profile({userID}) {
             <input {...register("name")} type="text" className="" placeholder="Enter your name" name="name"></input>
             <input {...register("address")} type="text" className="" placeholder="Enter your address" name="address"></input>
             <input {...register("contact")} type="text" className="" placeholder="Enter your mobile number" name="contact"></input>
-            <select {...register("type")} type="text" className=""  name="contact">
-                <option default disabled value="Choose your user type"></option>
+            <select {...register("type")} type="text" className="" placeholder="Select your user type"  name="type">
+                <option default disabled value="Select your user type"></option>
                 <option value="Restaurant"></option>
                 <option value="Grocery Store"></option>
             </select>
             <input {...register("userSince")} type="text" className="" hidden value={new Date().getFullYear()}/>
+            <button type="submit">Submit</button>
         </form>
         <p>{userData}</p>
     </div>
