6ac079295c9a58ce1af0913521f7c9821c717993
imvee <vind.veeru@gmail.com>
2022-06-05 21:28:59 +0530

    nesting forms

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -34,18 +34,21 @@ function Profile({userID}) {
     <div>
         <form id="main-form" onSubmit={handleSubmit(onSubmit)}></form>
         <form id="sub-form" onSubmit={handleImgUpload}></form>
-        
-        <input {...register("name")} type="text" className="" placeholder="Enter your name" name="name"></input>
-        <input {...register("address")} type="text" className="" placeholder="Enter your address" name="address"></input>
-        <input {...register("contact")} type="text" className="" placeholder="Enter your mobile number" name="contact"></input>
-        <select {...register("type")} type="text" className="" placeholder="Select your user type"  name="type">
-            <option default disabled value="Select your user type"></option>
+
+        <input {...register("name")} type="text" className="" placeholder="Enter your name" name="name" form="main-form"></input>
+        <input {...register("address")} type="text" className="" placeholder="Enter your address" name="address" form="main-form"></input>
+        <input {...register("contact")} type="text" className="" placeholder="Enter your mobile number" name="contact" form="main-form"></input>
+        <select {...register("type")} type="text" className="" placeholder="Select your user type"  name="type" form="main-form">
+            <option default disabled value="Select your user type" form="main-form"></option>
             <option value="Restaurant"></option>
             <option value="Grocery Store"></option>
         </select>
-        <input {...register("profileImg")} type="file" name="profileImg" />
-        <input {...register("userSince")} type="text" className="" hidden value={new Date().getFullYear()}/>
-        <button type="submit">Submit</button>
+
+        <input type="file" name="profileImg" form="sub-form"/>
+        <button type="submit" form="sub-form">Submit</button>
+
+        <input {...register("userSince")} type="text" className="" hidden value={new Date().getFullYear()} form="main-form"/>
+        <button type="submit" form="main-form">Submit</button>
         
     </div>
   )
