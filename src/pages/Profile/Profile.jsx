5a48093189b3a3d8bd07bb33b48641385d593b3b
imvee <vind.veeru@gmail.com>
2022-06-05 21:23:55 +0530

    nesting forms

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -26,21 +26,27 @@ function Profile({userID}) {
 
     }
 
+    const handleImgUpload=(e)=>{
+
+    }
+
   return (
     <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("name")} type="text" className="" placeholder="Enter your name" name="name"></input>
-            <input {...register("address")} type="text" className="" placeholder="Enter your address" name="address"></input>
-            <input {...register("contact")} type="text" className="" placeholder="Enter your mobile number" name="contact"></input>
-            <select {...register("type")} type="text" className="" placeholder="Select your user type"  name="type">
-                <option default disabled value="Select your user type"></option>
-                <option value="Restaurant"></option>
-                <option value="Grocery Store"></option>
-            </select>
-            <input {...register("profileImg")} type="file" name="profileImg" />
-            <input {...register("userSince")} type="text" className="" hidden value={new Date().getFullYear()}/>
-            <button type="submit">Submit</button>
-        </form>
+        <form id="main-form" onSubmit={handleSubmit(onSubmit)}></form>
+        <form id="sub-form" onSubmit={handleImgUpload}></form>
+        
+        <input {...register("name")} type="text" className="" placeholder="Enter your name" name="name"></input>
+        <input {...register("address")} type="text" className="" placeholder="Enter your address" name="address"></input>
+        <input {...register("contact")} type="text" className="" placeholder="Enter your mobile number" name="contact"></input>
+        <select {...register("type")} type="text" className="" placeholder="Select your user type"  name="type">
+            <option default disabled value="Select your user type"></option>
+            <option value="Restaurant"></option>
+            <option value="Grocery Store"></option>
+        </select>
+        <input {...register("profileImg")} type="file" name="profileImg" />
+        <input {...register("userSince")} type="text" className="" hidden value={new Date().getFullYear()}/>
+        <button type="submit">Submit</button>
+        
     </div>
   )
 }
