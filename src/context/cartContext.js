a155a80447a713c592dc94a192dac8037841e801
imvee <vind.veeru@gmail.com>
2022-06-06 16:10:09 +0530

    Context for cart

---
+++ src/context/cartContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+const initialState = {
+   shoppingList : []
+}
+
+export const CartContext = createContext(initialState);
+
+export const CartProvider = ({ children }) => {
+   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+   // Actions for changing state
+
+   function addItemToList(item) {
+       dispatch({
+           type: 'ADD_ITEM',
+           payload: item
+       });
+   }
+   function removeItemFromList(item) {
+       dispatch({
+           type: 'REMOVE_ITEM',
+           payload: item
+       });
+   }
+
+   return(
+      <CartContext.Provider value = {{shoppingList : state.shoppingList, addItemToList, removeItemFromList}}> 
+        {children} 
+      </CartContext.Provider>
+   )
+}
