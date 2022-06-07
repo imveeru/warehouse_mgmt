5d2c2abc5901e86c8f89e1d28004eab143ed0df2
imvee <vind.veeru@gmail.com>
2022-06-08 01:54:16 +0530

    Bill payment

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -2,6 +2,7 @@ import React,{useContext} from 'react'
 import {CartContext} from "./../../context/cartContext"
 import {db} from '../../firebase'
 import {useParams } from "react-router-dom"
+import toast, { Toaster } from 'react-hot-toast';
 
 function Checkout() {
 
@@ -11,13 +12,19 @@ function Checkout() {
   const { shoppingList }=useContext(CartContext)
   const billRef=db.collection("orders").doc(id)
   const pay=()=>{
-    billRef.update({hasPaid:true}).then(()=>{console.log("Bill Payed")}).catch((err)=>{console.log(err)})
+    billRef.update({hasPaid:true}).then(()=>{
+      console.log("Bill Payed")
+      toast("Bill Payed!",{icon:"🥳💵"})
+    }).catch((err)=>{
+      console.log(err)
+    })
   }
 
   return (
     <div>
+      <Toaster/>
       Checkout
-      {shoppingList.toString()}
+      <br></br><strong>Bill ID: </strong>{shoppingList.toString()}
       <p>{id}</p>
       <br/><button onClick={pay}>Pay</button>
     </div>
